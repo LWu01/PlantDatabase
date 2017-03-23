69cea80e079cad9e1806e95e85999aed4f0f0c2f
LWu01 <lijwu01@gmail.com>
2017-03-23 21:59:15 +0000

    Edit MainActivity, created Add and Update Class

--- app/src/main/java/com/lisa/currys/plantdatabase/MainActivity.java
+++ app/src/main/java/com/lisa/currys/plantdatabase/MainActivity.java
@@ -3,14 +3,19 @@ package com.lisa.currys.plantdatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView itemList;
-    DbHelper myDatabase;
-
+    private ListView itemList;
+    private DbHelper myDatabase;
+    private SimpleCursorAdapter adapter;
+    final String[] from = new String[] {PlantInfo._ID, PlantInfo.COL_NAME, PlantInfo.COL_SOW,
+            PlantInfo.COL_SOIL, PlantInfo.COL_WATER, PlantInfo.COL_TEMP, PlantInfo.COL_LIGHT,
+            PlantInfo.COL_HARVEST, PlantInfo.COL_EXTRA};
+    final int[] to = new int[]{}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
